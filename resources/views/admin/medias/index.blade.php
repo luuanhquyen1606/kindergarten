23f269598ddb3d4799f676ad783d3c2aa4980035
QuyenPC\admin <luuanhquyen@gmail.com>
2026-09-11 16:57:34 +0700

    no message

--- resources/views/admin/medias/index.blade.php
+++ resources/views/admin/medias/index.blade.php
@@ -1,14 +1,6 @@
 @extends('admin.layouts.app')
 @section('content')
 
-
-
-
-
-
-
-
-
 <style>
 
 .custom-modal{
@@ -29,11 +21,6 @@
 }
 
 </style>
-
-
-
-
-
 
 <div class="mb-9">
           <h2 class="mb-5">Medias</h2>
@@ -56,10 +43,10 @@
           </div>
           <div class="row g-3" > 
 
-            <?php for ($i=0; $i<=$per_page;$i++) { 
+            <?php for ($i=0; $i<$per_page;$i++) {
               ?> 
               
-                    <a style="cursor: pointer;"  data-bs-toggle="offcanvas" data-bs-target="#offcanvas_{{$i}}"  class="files col-6 col-sm-6 col-md-4 col-xl-2 hidden " id="file_{{$i}}"  >
+                    <a style="cursor: pointer;" href=""   data-gallery="gallery-class"  class="files col-6 col-sm-6 col-md-4 col-xl-2 hidden " id="file_{{$i}}"  >
                         
                         <div class="hoverbox img-zoom-hover rounded-2">
                             <img  srcset="" style="aspect-ratio:1 / 1;object-fit: cover;" class="src img-fluid" src="" alt="" />
@@ -68,51 +55,36 @@
                             </div>
                         </div>
                     </a>
-           
 
-
-             
-
-
-            <?php }
+<?php }
             ?>
             </div>
 
+</div>
 
-            
-           
-        </div>
-
-       
 <div style="padding: 10px;" data-list="" class="mb-9 card center col-auto d-flex">
                   <ul id="pagination" class="mb-0 pagination">
                   </ul>
 </div> 
 
+<input class="d-none"  id="feature_file_browser" type="file" multiple />
 
-
- <input class="d-none"  id="feature_file_browser" type="file" multiple />
-
-        <?php for ($i=0; $i<=48;$i++) { 
+        <?php for ($i=0; $i<$per_page;$i++) {
               ?> 
-              
-                    
 
-
-              <div class="offcanvas offcanvas-end" style="width:60% !important" id="offcanvas_{{$i}}" tabindex="-1" aria-labelledby="offcanvas_label">
+<div class="offcanvas offcanvas-end" style="width:60% !important" id="offcanvas_{{$i}}" tabindex="-1" aria-labelledby="offcanvas_label">
                                <div class="offcanvas-body "  >  
                                 
                                 <div style="margin-bottom: 10px;" class="overflow-hidden rounded">
 
                                     <img style="width: 100%;" src="" />
                                     <video class="video" id="video_{{$i}}" class="video" style="display: none; " height="600" controls>
-                                        <source class="video_source" src="/files/1/1779874547_2025-04-05-114749727.mp4" type="video/mp4">
+                                        <source class="video_source" src="" type="video/mp4">
                                     </video>
                                 </div> 
                                 <div class="col-12 col-sm-12">
-                                   
-                                   
-                                <h5 class="name"></h5>
+
+<h5 class="name"></h5>
                                         <div class="input-group mb-3">
                                             <input class="input_url form-control" type="text" disabled placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
                                             <button data-url="" class="copy_btn input-group-text" id="basic-addon2">copy</button>
@@ -120,34 +92,15 @@
                                    
                                 </div>
                             </div>
-                                <div class="offcanvas-body">
-                                
-                                </div>
                             </div>
 
-
-
-            <?php }
+<?php }
             ?>
-
-
- 
-
- 
-
-
-
-
-
-
-
-
 
 @endsection
 
 @section('js')
 <script>
-
 
 $('.copy_btn').on('click', function() {
     const text =$(this).attr('data-url');
@@ -161,28 +114,35 @@ $('.copy_btn').on('click', function() {
         });
 });
 
+var uploadedFiles = [];
 
-var uploadedFiles =[];
+// Pause any other playing video when one starts - bound once, not per file/render.
+$(document).on('play', '.video', function() {
+    $('.video').not(this).each(function() {
+        this.pause();
+    });
+});
+
 $('#feature_file_browser').on('change', function() {
     var files = this.files;
     if (files.length === 0) return;
 
-    var formData = new FormData();
-    uploadedFiles=[];
+    uploadedFiles = [];
     for (let i = 0; i < files.length; i++) {
         uploadSingleFile(files[i], function(response) {
-            uploadDone(files.length,i);
+            uploadDone(files.length, i);
         });
     }
 
+    // allow re-selecting the same file(s) again
+    this.value = '';
 });
 
-function uploadDone(all,uploaded){
-uploadedFiles.push(uploaded);
-    if(all == uploadedFiles.length){
+function uploadDone(all, uploaded) {
+    uploadedFiles.push(uploaded);
+    if (all == uploadedFiles.length) {
         loadFiles(1);
     }
-
 }
 
 function uploadSingleFile(file, callback) {
@@ -196,10 +156,11 @@ function uploadSingleFile(file, callback) {
         contentType: false,
         processData: false,
         success: function(response) {
-            callback(response);   // return here
+            callback(response);
         }
     });
 }
+
 function loadFiles(page = 1)
 {
 $.ajax({
@@ -210,27 +171,19 @@ $.ajax({
         $('.files').addClass("hidden");
         let files = response.files.data;
         $.each(files, function(index, file) {
-            $('#file_'+index+' img').attr('src', file.path);
-            $('#file_'+index+' img').attr('srcset', file.srcset);
-            $('#file_'+index+' .name').html(file.original_name);
-            // preview media
-            $('#offcanvas_'+index+' .name').html(file.original_name); 
-            $('#offcanvas_'+index+' img').attr('src', file.path);
-            $('#offcanvas_'+index+' img').attr('srcset', file.srcset);
-            $('#offcanvas_'+index+' .copy_btn').attr('data-url', "{{ url('/') }}"+file.path);
-            $('#offcanvas_'+index+' .input_url').val("{{ url('/') }}"+file.path); 
-            $('#file_'+index).removeClass("hidden");
-            if (file.mime_type.includes("video")) { 
-                $('#offcanvas_'+index+' .video_source').attr('src', "{{ url('/') }}"+file.path);
-                $('#offcanvas_'+index+' img').hide();
-                $('#offcanvas_'+index+' video').show(); 
-                $('#offcanvas_'+index+' video')[0].load();
-                $('.video').on('play', function() {
-                    $('.video').not(this).each(function() {
-                        this.pause();
-                    });
-                });
-            }
+            let $file = $('#file_'+index);
+            let $offcanvas = $('#offcanvas_'+index);
+            let fileUrl = "{{ url('/') }}"+file.path;
+            
+            $file.find('img').attr('src', file.thumbnailUrl);
+            $file.find('.name').text(file.original_name);
+            $file.attr('href', file.thumbnailUrl);
+            //
+            //data-gallery="gallery-class"
+
+            $file.removeClass("hidden");
+
+            
         });
         buildPagination(response.files);
     },
@@ -239,7 +192,6 @@ $.ajax({
     }
 });
 }
-
 
 function buildPagination(files)
 {
